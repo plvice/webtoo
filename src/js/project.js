111a56a9f1db491e0e6499b19b29727d128762f7
plvice <prawdziwypiotrek@gmail.com>
2016-04-11 01:24:08 +0200

    changed project module

--- src/js/project.js
+++ src/js/project.js
@@ -13,15 +13,31 @@ var ProjectModule = core.modules.project = {
     },
     dataApi: '/api/wp-json/wp/v2/project/',
     attachmentsApi: 'api//wp-json/wp/v2/media?parent=10&media_type=image',
-    activeDropdown: null
+    activeDropdown: null,
+    activeItem: null
 };
 
+// można się zastanowić na usunięciem tego
 ProjectModule.buildTemplate = function (data) {
     var template = core.templates.project;
     var html = template(data);
-    console.log(html);
-
+    return html;
 };
+
+ProjectModule.insertItemContent = function (html) {
+    var _module = this;
+    var parent = _module.activeItem.parentNode;
+    var dropdown = parent.getElementsByClassName(_module.cls.dropdown);
+
+    //add dropdown: only if not exists for this project
+    if (!dropdown.length) {
+        //można wykorzystać metodę z core.js
+        var node = document.createElement('div');
+        node.setAttribute('class', 'dropdown');
+        node.innerHTML = html;
+        parent.appendChild(node);
+    }
+}
 
 ProjectModule.loadItem = function (id) {
     var _module = this;
@@ -31,7 +47,11 @@ ProjectModule.loadItem = function (id) {
 
         if (data.readyState === 4 && json !== '') {
             json = JSON.parse(json);
-            _module.buildTemplate(json);
+
+            //można tu bezpośrednio wywołać szablon - będzie jedna metoda mniej
+            var html = _module.buildTemplate(json);
+            _module.insertItemContent(html);
+            _module.selectItem(_module.activeItem);
         }
     };
 
@@ -49,6 +69,7 @@ ProjectModule.loadItem = function (id) {
     }
 }
 
+//tu chyba bardziej pasuje nazwa showItem
 ProjectModule.selectItem = function (item) {
     var _module = this,
         allowToLoad = true;
@@ -77,6 +98,7 @@ ProjectModule.selectItem = function (item) {
 
         //set current dropdown height
         _module.setDropdownHeight(parent);
+        // _module.loadItem();
     }
 
     //return boolean - for checking the state inside the bindItemEvents method
@@ -86,7 +108,7 @@ ProjectModule.selectItem = function (item) {
 ProjectModule.setDropdownHeight = function (parent) {
     var _module = this;
     var activeDropdown = _module.activeDropdown;
-
+    console.log(parent);
     var dropdown = parent.getElementsByClassName(_module.cls.dropdown)[0];
     var dropdownContainer = dropdown.getElementsByClassName(_module.cls.dropdownContainer)[0];
     var height = core.pixelize(dropdownContainer.offsetHeight);
@@ -106,18 +128,22 @@ ProjectModule.setDropdownHeight = function (parent) {
 ProjectModule.bindItemEvents = function () {
     var _module = this;
     var items = _module.el.projectItems;
-    var idAttr,
-        isToLoad;
+    var id,
+        isActive;
 
     var clickFunction = function (e) {
         e.preventDefault();
 
-        idAttr = this.getAttribute('data-id');
-        isToLoad = _module.selectItem(this);
+        id = this.getAttribute('data-id');
+        // isToLoad = _module.selectItem(this);
+
+        isActive = this.parentNode.classList.contains(_module.cls.columnExpanded);
+        console.log('is active: ' + isActive);
 
         //check if data-id exists and clicked item is not active
-        if (idAttr && isToLoad) {
-            _module.loadItem(idAttr);
+        if (id && !isActive) {
+            _module.activeItem = this;
+            _module.loadItem(id);
         }
     };
 
